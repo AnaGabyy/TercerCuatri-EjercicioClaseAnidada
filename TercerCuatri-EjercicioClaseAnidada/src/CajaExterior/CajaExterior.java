f7ce4f06ec107d9cfcade6130742200f2be67df4
Ana Gabriela <112995260+AnaGabyy@users.noreply.github.com>
2023-05-08 02:28:06 -0300

    Refactorización: implementación de la interfaz mostrar()

--- TercerCuatri-EjercicioClaseAnidada/src/CajaExterior/CajaExterior.java
+++ TercerCuatri-EjercicioClaseAnidada/src/CajaExterior/CajaExterior.java
@@ -1,30 +1,30 @@
 package CajaExterior;
 
 //Clase CajaExterior (OuterClass)
-public class CajaExterior {
+public class CajaExterior implements Mostrar{
 
-        private int x = 10;
+    private int x = 10;
 
-        // Función para mostrar el valor de x
-        public void mostrarX() {
-            System.out.println("El valor de x es " + x);
+    // Función para mostrar el valor de x
+    @Override
+    public void mostrar() {
+        System.out.println("El valor de x es " + x);
+    }
+
+    // Función para crear una nueva CajaInterior
+    public CajaInterior crearInterior() {
+        return new CajaInterior();
+    }
+
+    // Clase dentro de Caja exterior usada como beneficio de que no se sabe como es el valor de "y"
+    /*InnerClass. La clase interna debe ser privada, de este modo cuando tenga que llamarla debo
+    llamar primero a la clase externa (la contenedora)*/
+    public class CajaInterior implements Mostrar {
+        private int y = 20;
+        @Override
+        public void mostrar() {
+            System.out.println("El valor de y es " + y);
         }
-
-        // Función para crear una nueva CajaInterior
-        public CajaInterior crearInterior() {
-            return new CajaInterior();
-        }
-
-        // Clase dentro de Caja exterior usada como beneficio de que no se sabe como es el valor de "y"
-        /*InnerClass. La clase interna debe ser privada, de este modo cuando tenga que llamarla debo
-        llamar primero a la clase externa (la contenedora)*/
-        private class CajaInterior {
-            private int y = 20;
-
-            // Función para mostrar el valor de y
-            public void mostrarY() {
-                System.out.println("El valor de y es " + y);
-            }
-        }
+    }
 
 }
